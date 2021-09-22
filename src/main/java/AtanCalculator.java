a37f62f3e6398e3aa3cb45e21a55930a0e57a8cb
user1 <anastasia.d.sokolova@gmail.com>
2021-09-22 10:31:39 +0300

    fix math

--- src/main/java/AtanCalculator.java
+++ src/main/java/AtanCalculator.java
@@ -7,47 +7,30 @@ public final class AtanCalculator {
 
 
     public static Double calculateAtan(double x, double maxError)  {
-            if (abs(maxError) < MIN_ERROR || isNaN(maxError)) {
-                return atanDecompose(x, getOptimalNLength(x, MIN_ERROR));
-                }
-            else if(isNaN(x)) {  return NaN; }
-            else return atanDecompose(x, getOptimalNLength(x, maxError));
-            }
-
-
-        private static double atanDecompose(double x, int repeats) {
-            if (Math.abs(x) > 1) {
-                return signum(x) * PI/2 - atanDecompose(1 / x, repeats);
-            } else {
-                double result = 0.0D;
-                for (int n=0;  n<=repeats; n++) {
-                    result +=  pow(-1.0, n) * pow(x, (2 * n + 1)) / (2 * n + 1);
-                    }
-                return result;
-                }
-            }
-
-
-        private static int getOptimalNLength(double x, double maxError) {
-            if (abs(x) > 1) {
-                return getOptimalNLength(1/x, maxError);
-            }
-            else {
-                int min = 0;
-                int max = Integer.MAX_VALUE;
-                int n = 0;
-
-                while(min < max) {
-                    n = min / 2 + max / 2;
-                    double trial = pow(x,(2 * n + 1)) / (2 * n + 1);
-                    if (abs(trial) > abs(maxError)) {
-                        min = n + 1;
-                    } else {
-                        max = n;
-                    }
-                }
-                return n;
-            }
+        if (abs(maxError) < MIN_ERROR || isNaN(maxError)) {
+            return atanDecompose(x, MIN_ERROR);
         }
-
+        else if(isNaN(x)) {  return NaN; }
+        else return atanDecompose(x, maxError);
     }
+
+
+    private static double atanDecompose(double x, double repeats) {
+        if (abs(x) > 1) {
+            return signum(x) * PI/2 - atanDecompose(1 / x, repeats);
+        } else {
+            boolean stop = false;
+            double result = 0.0D;
+            int n=0;
+            while(!stop) {
+                result +=  pow(-1.0, n) * pow(x, (2 * n + 1)) / (2 * n + 1);
+                double trial = pow(x,(2 * n + 1)) / (2 * n + 1);
+                if (abs(trial) > abs(repeats)) {
+                    n+=1;
+                }
+                else stop = true;
+            }
+            return result;
+        }
+    }
+}
